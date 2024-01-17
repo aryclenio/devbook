d3a078317e968349c9e607bbec4e759f3f7b7c65
aryclenio <aryclenio.barros@x-team.com>
2024-01-17 13:55:13 -0300

    🚚 Updating routes

--- api/src/router/routes/routes.go
+++ api/src/router/routes/routes.go
@@ -1,10 +1,23 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 type Route struct {
 	URI                   string
 	Method                string
 	Func                  func(http.ResponseWriter, *http.Request)
 	RequireAuthentication bool
+}
+
+func Configure(r *mux.Router) *mux.Router {
+	routes := userRoutes
+
+	for _, route := range routes {
+		r.HandleFunc(route.URI, route.Func).Methods(route.Method)
+	}
+	return r
 }
